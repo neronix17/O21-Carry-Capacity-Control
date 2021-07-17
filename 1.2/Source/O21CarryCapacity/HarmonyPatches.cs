1085a2162e50d90c1252dfca9f77d6228568fcfc
neronix17 <39186138+neronix17@users.noreply.github.com>
2021-07-17 19:35:58 +0100

    init

---
+++ 1.2/Source/O21CarryCapacity/HarmonyPatches.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+using HarmonyLib;
+
+namespace O21CarryCapacity
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            Harmony harmony = new Harmony("neronix17.utility.carrycapcontrol");
+            harmony.PatchAll();
+        }
+
+    }
+
+    [HarmonyPatch(typeof(MassUtility), "Capacity")]
+    [HarmonyPriority(999)]
+    public static class MassUtility_Capacity_Prefix
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref float __result, ref Pawn p, ref StringBuilder explanation)
+        {
+            if (!MassUtility.CanEverCarryAnything(p))
+            {
+                __result = 0f;
+            }
+            else
+            {
+                float num = StatDefOf.CarryingCapacity.defaultBaseValue * p.BodySize * CarryCapMod.settings.capacityMultiplier  * Current.Game.Scenario.GetStatFactor(StatDefOf.CarryingCapacity);
+                if (explanation != null)
+                {
+                    if (explanation.Length > 0)
+                    {
+                        explanation.AppendLine();
+                    }
+                    explanation.Append("  - " + p.LabelShortCap + ": " + num.ToStringMassOffset());
+                }
+                __result = num;
+            }
+            return false;
+        }
+    }
+}
---
+++ 1.3/Source/O21CarryCapacity/CarryCapMod.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace O21CarryCapacity
+{
+    public class CarryCapMod : Mod
+    {
+        public static CarryCapSettings settings;
+        public static CarryCapMod mod;
+
+        public CarryCapMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<CarryCapSettings>();
+            mod = this;
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Carry Capacity Control";
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.Label("Capacity Multiplier");
+            listingStandard.GapLine();
+            listingStandard.Label("This multiplies the base carry capacity of a pawn.");
+            listingStandard.Label("Default: 0.50, Min-Max: 0.05 - 5.00, Current: " + settings.capacityMultiplier.ToString("0.00"));
+            settings.capacityMultiplier = listingStandard.Slider(settings.capacityMultiplier, 0.05f, 5.00f);
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+    }
+
+    public class CarryCapSettings : ModSettings
+    {
+        public float capacityMultiplier = 0.5f;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref capacityMultiplier, "capacityMultiplier", 0.5f);
+        }
+    }
+}
